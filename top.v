6883f4de9f4288bd777dcc05c70321df9a655b6d
Mohammedsgf <50026324+mohammedsgf@users.noreply.github.com>
2020-01-20 16:21:45 +0300

    Update top.v

--- top.v
+++ top.v
@@ -9,7 +9,7 @@ wire Clk_2Hz;
 
 assign LED={4'b0000,LC,LB,LA,RA,RB,RC};
 
-TailLight(Clk_2Hz,~reset,LEFT,RIGHT,HAZ,LC,LB,LA,RA,RB,RC);
+TailLight(Clk_2Hz,LEFT,RIGHT,HAZ,LC,LB,LA,RA,RB,RC);
 Clock_divider(Clk_50MHz,Clk_2Hz,~reset);
 
 
